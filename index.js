5c559c456293e6ba11fe2b566b90b9b4e8873020
fedghsjd <gs1aileeno1@hotmail.com>
2024-05-08 12:39:44 +0700

    test

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const lcmRecursive = (a, b) => {
-  return (a * b) / gcdRecursive(a, b);
-};
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 1; i <= n + 1; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
+}
