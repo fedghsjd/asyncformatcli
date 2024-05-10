0a1df9cc2476b6eff96080ff89bc38cd620eaeb3
fedghsjd <gs1aileeno1@hotmail.com>
2024-05-10 15:55:43 +0700

    feat project

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function findMaxLength(nums) {
-  const map = new Map();
-  map.set(0, -1);
-  let count = 0;
-  let maxLength = 0;
-  for (let i = 0; i < nums.length; i++) {
-    count += nums[i] === 1 ? 1 : -1;
-    if (map.has(count)) {
-      maxLength = Math.max(maxLength, i - map.get(count));
-    } else {
-      map.set(count, i);
-    }
-  }
-  return maxLength;
-}
+const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);
